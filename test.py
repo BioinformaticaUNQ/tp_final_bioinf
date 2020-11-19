9dd495da294a47b050e2bc3c2db5e67d076b462c
Gaston Veliez <gastonveliez95@gmail.com>
2020-11-19 01:22:30 -0300

    Modificaciones para mostrar datos de los resultados blast

--- test.py
+++ test.py
@@ -18,7 +18,16 @@ def fasta():
     urllib.request.urlretrieve(url2, pdb_id + ".fasta")
     fasta_string = open(pdb_id + ".fasta").read()
     print(fasta_string)
-    result_handle = NCBIWWW.qblast("blastp", "pdb", fasta_string)
+    result_handle = NCBIWWW.qblast("blastp", "pdb", fasta_string.split('\n')[1],
+        short_query=True,
+        hitlist_size=50,
+        expect=1000,
+        word_size=7,
+        nucl_reward=1,
+        matrix_name='BLOSUM62',
+        nucl_penalty=-3,
+        threshold=0.05,
+        gapcosts="11 1")
     blast = pdb_id + ".xml"
 
     save_clk = open(blast, "w")
@@ -27,6 +36,21 @@ def fasta():
     
     blast_records = NCBIXML.parse(open(blast))
     print(blast_records)
+    results = []
+    for blast_record in blast_records:
+        for alignment in blast_record.alignments:
+            aligned_sequence = ''
+            aligned_matches = ''
+            for hsp in alignment.hsps:
+                aligned_sequence = aligned_sequence + hsp.sbjct
+                aligned_matches = aligned_matches + hsp.match
+
+            print(alignment.title.split('>')[0])
+            print(aligmened_sequence.replace('-', ''))
+            print(aligmened_matches)
+            print(aligmened_sequence)
+            
+
 
 myLabel = Label(root, text="Ingresar codigo PDB")
 myLabel.pack()
