b81624f113d6122f82476d936fb3bd41b794d64e
Esteban Matas <estebanmatas13@gmail.com>
2020-11-28 18:53:33 -0300

    Modificado main para poder seleccionar secuencias especificas

--- main.py
+++ main.py
@@ -9,37 +9,102 @@ import logomaker as lm
 import matplotlib.pyplot as plt
 import os
 from PIL import Image,ImageTk
+from functools import partial
 
 root = Tk()
 root.title("TP FINAL")
 root.geometry("1920x1080")
 
-def blast_query(fasta_string,pdb_id):
-    result_handle = NCBIWWW.qblast("blastp", "pdb", fasta_string.split('\n')[1],alignments=10,hitlist_size=10)
+def getPDB():
+    pdb_id = str(myTextbox.get())
+    url = "https://files.rcsb.org/download/"+ pdb_id +".pdb"
+    try:
+        urllib.request.urlretrieve(url, pdb_id + ".pdb")
+    except Exception as e:
+        print(str(e))
+        if(str(e) == "HTTP Error 404: Not Found"):
+            messagebox.showerror("Error", "Código pdb inválido")
+        else:
+            messagebox.showerror("Error", "Hubo un error al obtener la proteína")
+        return
+
+    getFASTA(pdb_id)
+
+def getFASTA(pdb_id):
+    url2 = "https://www.rcsb.org/fasta/entry/"+pdb_id+"/download"
+    urllib.request.urlretrieve(url2, pdb_id + ".fasta")
+    getSequencesFromPDB(pdb_id)
+
+def getSequencesFromPDB(pdb_id):
+    fasta_string = open(pdb_id + ".fasta").read()
+    sequences = []
+    counter = 0
+
+    for line in fasta_string.splitlines():
+        if(oddNumber(counter)):
+            sequences.append(line)
+        counter += 1
+    
+    putNumberOfSequencesLabel(pdb_id, sequences)
+
+def oddNumber(number):
+    return number % 2 != 0
+
+def putNumberOfSequencesLabel(pdb_id, sequences):
+    if len(sequences) > 1:
+        numberOfSequenceLabel = Label(root, text = "El código " + pdb_id + " tiene " + str(len(sequences)) + " secuencias")
+        numberOfSequenceLabel.pack(pady = 5)
+    else:
+        numberOfSequenceLabel = Label(root, text = "El código " + pdb_id + " tiene 1 sola secuencia")
+        numberOfSequenceLabel.pack(pady = 5)
+
+    putSequencesLabelAndButtonsToChoose(pdb_id, sequences)
+
+def putSequencesLabelAndButtonsToChoose(pdb_id, sequences):
+    selectSequenceLabel = Label(root, text="Seleccione la secuencia a procesar")
+    selectSequenceLabel.pack()
+
+    for sequence in sequences:
+        sequenceLabel = Button(root, text= sequence, command = lambda sequence = sequence : partial(blast_query(sequence, pdb_id)))
+        sequenceLabel.pack(pady = 5)
+
+def blast_query(sequence,pdb_id):
+    print("Eligio la secuencia " + sequence)
+    result_handle = NCBIWWW.qblast("blastp", "pdb", sequence, alignments=10,hitlist_size=10)
     blast = pdb_id + ".xml"
+
     with open(blast, "w") as out_handle:
         out_handle.write(result_handle.read())
 
     result_handle.close()
-    return blast
+    runClustal(pdb_id, blast)
+    
+def runClustal(pdb_id, blast):
+    blast_records = NCBIXML.parse(open(blast))
+    if not os.path.exists("./fasta"):
+        os.mkdir("./fasta")
+    
+    all_seq_fasta = "./fasta/"+pdb_id+".fasta"
+    file = open(all_seq_fasta, "w")
+    file.writelines(fasta_string.split('\n')[0] + '\n')
+    file.writelines(fasta_string.split('\n')[1] + '\n')
 
-def generate_3structure(pdb_id):
-    pymol.cmd.load(pdb_id + ".pdb", pdb_id)
-    pymol.cmd.disable("all")
-    pymol.cmd.enable(pdb_id)
-    print(pymol.cmd.get_names())
-    pymol.cmd.hide('all')
-    pymol.cmd.show('cartoon')
-    pymol.cmd.set('ray_opaque_background', 0)
-    pymol.cmd.color('red', 'ss h')
-    pymol.cmd.color('yellow', 'ss s')
-    pymol.cmd.png("%s.png"%(pdb_id))
-    #pymol.cmd.quit()
-    loadPymol = Image.open(pdb_id + ".png")
-    renderPymol = ImageTk.PhotoImage(loadPymol)
-    imgPymol = Label(root,image=renderPymol)
-    imgPymol.image = renderPymol
-    imgPymol.place(x=0,y=400)
+    for blast_record in blast_records:
+        for alignment in blast_record.alignments:
+            file.writelines(">" + alignment.hit_id)
+            file.writelines("\n")
+            for hsp in alignment.hsps:
+                sbjct_no_gaps = hsp.sbjct.replace("-","")
+                file.writelines(sbjct_no_gaps)
+                file.writelines("\n")
+    
+    file.close()
+    outputPath = "./fasta/"+pdb_id+"_aln.fasta"
+    clustalomega_cline = ClustalOmegaCommandline(infile = all_seq_fasta, outfile = outputPath,force = True)
+    clustalomega_cline()
+    
+    generate_alignment_view(outputPath,pdb_id)
+    generate_3structure(pdb_id)
 
 def generate_alignment_view(outputPath,pdb_id):
     raw_seqs =[]
@@ -69,53 +134,24 @@ def generate_alignment_view(outputPath,pdb_id):
     img.image = render
     img.place(x=0,y=80)
 
-def fasta():
-    pdb_id = str(myTextbox.get())
-    url = "https://files.rcsb.org/download/"+ pdb_id +".pdb"
-    try:
-        urllib.request.urlretrieve(url, pdb_id + ".pdb")
-    except Exception as e:
-        print(str(e))
-        if(str(e) == "HTTP Error 404: Not Found"):
-            messagebox.showerror("Error", "Código pdb inválido")
-        else:
-            messagebox.showerror("Error", "Hubo un error al obtener la proteína")
-        return
-    url2 = "https://www.rcsb.org/fasta/entry/"+pdb_id+"/download"
-    urllib.request.urlretrieve(url2, pdb_id + ".fasta")
-    fasta_string = open(pdb_id + ".fasta").read()
-    print(fasta_string)
+def generate_3structure(pdb_id):
+    pymol.cmd.load(pdb_id + ".pdb", pdb_id)
+    pymol.cmd.disable("all")
+    pymol.cmd.enable(pdb_id)
+    print(pymol.cmd.get_names())
+    pymol.cmd.hide('all')
+    pymol.cmd.show('cartoon')
+    pymol.cmd.set('ray_opaque_background', 0)
+    pymol.cmd.color('red', 'ss h')
+    pymol.cmd.color('yellow', 'ss s')
+    pymol.cmd.png("%s.png"%(pdb_id))
+    #pymol.cmd.quit()
+    loadPymol = Image.open(pdb_id + ".png")
+    renderPymol = ImageTk.PhotoImage(loadPymol)
+    imgPymol = Label(root,image=renderPymol)
+    imgPymol.image = renderPymol
+    imgPymol.place(x=0,y=400)
     
-    blast = blast_query(fasta_string,pdb_id)
-
-    blast_records = NCBIXML.parse(open(blast))
-
-    owd = os.getcwd()
-    if not os.path.exists("./fasta"):
-        os.mkdir("./fasta")
-    all_seq_fasta = "./fasta/"+pdb_id+".fasta"
-    file = open(all_seq_fasta, "w")
-    file.writelines(fasta_string.split('\n')[0] + '\n')
-    file.writelines(fasta_string.split('\n')[1] + '\n')
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
-            file.writelines(">" + alignment.hit_id)
-            file.writelines("\n")
-            for hsp in alignment.hsps:
-                sbjct_no_gaps = hsp.sbjct.replace("-","")
-                file.writelines(sbjct_no_gaps)
-                file.writelines("\n")
-    file.close()
-    outputPath = "./fasta/"+pdb_id+"_aln.fasta"
-    clustalomega_cline = ClustalOmegaCommandline(infile = all_seq_fasta, outfile = outputPath,force = True)
-    clustalomega_cline()
-    
-    generate_alignment_view(outputPath,pdb_id)
-
-    generate_3structure(pdb_id)
-    
-            
-
 
 myLabel = Label(root, text="Ingresar codigo PDB")
 myLabel.pack()
@@ -123,7 +159,7 @@ myLabel.pack()
 myTextbox = Entry(root, width=50)
 myTextbox.pack()
 
-myButton = Button(root, text="Procesar", command = fasta)
-myButton.pack()
+myButton = Button(root, text="Procesar", command = getPDB)
+myButton.pack(pady = (0,30))
 
 root.mainloop()
