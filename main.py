e37c699e36b0547b652fd8b83a22bd18ba1911e3
Gaston Veliez <gastonveliez95@gmail.com>
2020-12-08 17:07:13 -0300

    Refactor de codigo, documentacion y separacion en metodos (#6)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from tkinter import messagebox, ttk
 import urllib.request
-import logomaker as lm
-import matplotlib.pyplot as plt
 import os
 from services import pymol_service,blast_service,clustal_service,logomaker_service,dssp_service
 from PIL import Image,ImageTk
@@ -11,8 +9,9 @@ import numpy as np
 from datetime import datetime
 from pandas import DataFrame
 import logging
-from threading import Thread
 import math
+
+#Configuracion de UI con scrollbars
 
 root = Tk()
 root.title("TP FINAL")
@@ -21,22 +20,24 @@ pdbs_to_process =[]
 
 evalue = DoubleVar(value=0.004)
 coverage = IntVar(value=80)
-#SCROLLBAR
+
 mainFrame = Frame(root)
 mainFrame.pack(fill = BOTH, expand = 1)
 myCanvas = Canvas(mainFrame)
 myCanvas.pack(side = LEFT, fill = BOTH, expand = 1)
 myScrollBar = ttk.Scrollbar(mainFrame, orient = VERTICAL, command = myCanvas.yview)
+myScrollBarHorizontal = ttk.Scrollbar(mainFrame, orient = HORIZONTAL, command = myCanvas.xview)
+myScrollBarHorizontal.pack(side=BOTTOM,fill= X)
 myScrollBar.pack(side = RIGHT, fill = Y)
 myCanvas.configure(yscrollcommand = myScrollBar.set)
-myCanvas.bind('<Configure>', lambda e : myCanvas.configure(scrollregion = myCanvas.bbox("all")))
+myCanvas.bind('<Configure>', lambda e : myCanvas.configure(scrollregion = myCanvas.bbox("all"),yscrollcommand=myScrollBar.set, xscrollcommand=myScrollBarHorizontal.set))
 secondFrame = Frame(myCanvas)
 myCanvas.create_window((0, 0), window = secondFrame, anchor = "nw")
 
 progress_bar = ttk.Progressbar(secondFrame,orient=HORIZONTAL,maximum=100)
 search_label = Label(secondFrame,text="Por favor, espere...")
 
-#DESCARGA BDD
+#Descarga base de datos PDB en la primera ejecucion
 if not os.path.exists("./db"):
         os.mkdir("./db")
 if not os.path.isfile("./db/pdbaa.pdb"):
@@ -52,6 +53,7 @@ if not os.path.isfile("./db/pdbaa.pdb"):
         messagebox.showerror("Error", "Hubo un error al obtener la base de datos pdb")
         exit()
 
+#Descarga pdb del pdbid ingresado. Valida que sea correcto
 def getPDB():
     input_path = "./ejecucion-" + datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     os.mkdir(input_path)
@@ -70,6 +72,7 @@ def getPDB():
 
     getFASTA(pdb_id,input_path)
 
+#Descarga fasta del pdb ingresado
 def getFASTA(pdb_id,input_path):
     url2 = "https://www.rcsb.org/fasta/entry/"+pdb_id+"/download"
     urllib.request.urlretrieve(url2, pdb_id + ".fasta")
@@ -124,7 +127,7 @@ def putSequencesLabelAndButtonsToChoose(pdb_id, dataAndSequencesMap, rna,input_p
         seq_label = Label(secondFrame,text = v)
         chain_label.pack(pady = (30, 0))
         seq_label.pack(pady = (30, 0))
-        Button(secondFrame, text = "Ejecutar cadena "+str(index), command = lambda k = k, v = v : start_query(pdb_id, k, v,input_path)).pack(pady = 5)
+        Button(secondFrame, text = "Ejecutar cadena "+str(index), command = lambda k = k, v = v : blast_query(pdb_id, k, v,input_path)).pack(pady = 5)
 
     if len(rna) > 1:
         rnaLabel = Label(secondFrame, text = "Se omitieron " + str(len(rna)) + " secuencias de RNA")
@@ -133,34 +136,35 @@ def putSequencesLabelAndButtonsToChoose(pdb_id, dataAndSequencesMap, rna,input_p
         rnaLabel = Label(secondFrame, text = "Se omitió 1 secuencia de RNA")
         rnaLabel.pack(pady = (30, 0))
 
-
-def start_query(pdb_id, data, sequence,input_path):
-    t= Thread(target=blast_query(pdb_id, data, sequence,input_path))
-    t.start()
-
+#Busqueda de proteinas homologas con blastp
 def blast_query(pdb_id, data, sequence,input_path):
     search_label.pack(pady=(0,30))
     progress_bar.pack(pady=(0,30))
     progress_bar.start()
     root.update_idletasks()
+
     logging.info("Eligio la secuencia " + sequence)    
     logging.info("Se ejecuta la busqueda de proteinas homologas con los siguientes parametros:")
     logging.info("Porcentaje de identidad: >40%")
     logging.info("Porcentaje de coverage: >" + str(coverage.get()))
     logging.info("eValue: " + str(evalue.get()))
     logging.info("El resto de los valores son estandares de blastp") #Poner link de doc
+
     fasta_seq = blast_service.blastp_query(pdb_id,evalue.get(),coverage.get(),data,sequence)
-    progress_bar.step(25)
-    root.update_idletasks()
+    define_progress(25)
     align_and_generate_structures(pdb_id, fasta_seq, sequence, data,input_path)
     
+#Alineamiento de estructura primaria con proteinas homologas usando clustal
 def align_and_generate_structures(pdb_id, fasta_seq, sequence, data,input_path):
+    logging.info("Se ejecuto un alineamiento multiple de la cadena problema junto con las proteinas homologas obtenidas previamente")
+    logging.info("Esto se ejecuto con Clustal Omega, utilizando como parametro un archivo fasta con todas las cadenas y con el resto de valores por default")
     output_path = clustal_service.run_clustal(pdb_id,fasta_seq,input_path)
-    progress_bar.step(50)
-    root.update_idletasks()
+    define_progress(50)
     generate_alignment_view(output_path,pdb_id,input_path)
     generate_3structure(pdb_id,input_path)
 
+#Genera el alineamiento de las estructuras secundarias.
+#Genera graficos con logomaker para mostrar los grados de conservacion de la estructura secundaria y primaria
 def generate_alignment_view(outputPath,pdb_id,input_path):
     pdbs = []
     raw_seqs =[]
@@ -180,8 +184,6 @@ def generate_alignment_view(outputPath,pdb_id,input_path):
     pdbs.append(pdb_id)
     pdbs_to_process.extend(pdbs[-10:])
     second_structure_fasta = dssp_service.generate_2structures(pdbs_to_process,input_path,pdb_id)
-    # primary_map = get_primary_map(input_path + '/' + pdb_id + '_aln.fasta', pdb_id)
-    # secondary_fasta = dssp_service.generate_secondary_fasta(primary_map, input_path)
     raw_seqs2 =[]
     with open(second_structure_fasta, "r") as f:
         seqText = ""
@@ -194,81 +196,45 @@ def generate_alignment_view(outputPath,pdb_id,input_path):
             else:
                 seqText += sq
     seqs2 = [seq.strip() for seq in raw_seqs2 if ('#' not in seq) and ('>') not in seq]
-    
-    
-    counts_mat = lm.alignment_to_matrix(seqs)
-    divider = len(seqs[0]) / 35
-    counts_mat_list = np.array_split(counts_mat, math.ceil(divider))
-    
-    counts_mat2 = lm.alignment_to_matrix(seqs2)
-    divider2= len(seqs2[0]) / 40
-    counts_mat_list2 = np.array_split(counts_mat2, math.ceil(divider2))
+    logomaker_service.primary_structure_conservation(seqs,pdb_id,secondFrame,logging)
+    logomaker_service.secondary_structure_conservation(seqs2,pdb_id,secondFrame,logging)
+    define_progress(75)
 
-    alignment_label = Label(secondFrame,text="Alineamiento de estructura primaria")
-    alignment_label.pack(pady=(0,30))
-    for df in counts_mat_list:
-        crp_logo = lm.Logo(df,color_scheme='skylign_protein')
 
-        # style using Axes methods
-        crp_logo.ax.xaxis.set_ticks_position('none')
-        crp_logo.ax.xaxis.set_tick_params(pad=-1)
-        plt.savefig(input_path + "/" + pdb_id + "_aln.png")
-        load = Image.open(input_path + "/" + pdb_id + "_aln.png")
-        render = ImageTk.PhotoImage(load)
-        img = Label(secondFrame,image=render)
-        img.image = render
-        img.pack(pady = (30, 0))
-
-    alignment_label2 = Label(secondFrame,text="Alineamiento de estructura secundaria")
-    alignment_label2.pack(pady=(0,30))
-    for df in counts_mat_list2:
-        crp_logo = lm.Logo(df)
-        # style using Axes methods
-        crp_logo.ax.xaxis.set_ticks_position('none')
-        crp_logo.ax.xaxis.set_tick_params(pad=-1)
-        plt.savefig(pdb_id + "_aln_secondary.png")
-        load = Image.open(pdb_id + "_aln_secondary.png")
-        render = ImageTk.PhotoImage(load)
-        img = Label(secondFrame,image=render)
-        img.image = render
-        img.pack(pady = (30, 0))
-    progress_bar.step(75)
-    root.update_idletasks()
-
-def get_primary_map(filePath, pdb_id):
-    seq_map = {}
-    with open(filePath, "r") as f:
-            seqText = ""
-            key = ""
-
-            for sq in f:
-                if '>' in sq:
-                    if seqText != "":
-                        seq_map[key] = seqText
-                        seqText = "" 
-                    if pdb_id not in sq:
-                        seq_map[sq.split("|")[1]] = ''
-                        key = sq.split("|")[1]
-                    else:
-                        seq_map[pdb_id] = ''
-                        key = pdb_id
-
-                else:
-                    seqText += sq.replace('\n',"")
-            seq_map[key] = seqText
-    return seq_map
-
+#Genera alineamiento de las estructuras secundarias con pymol
 def generate_3structure(pdb_id,input_path):
-    progress_bar.step(90)
-    root.update_idletasks()
+    define_progress(90)
+
+    logging.info("Se corre Pymol con las proteínas: " + str(pdbs_to_process))
+    logging.info("Se utilizan los comandos fetch , alignto y el formato cartoon para el gráfico")
+
     pymol_service.generate_3structure_image(pdb_id,pdbs_to_process,input_path)
+
+    logging.info("Se generó un espacio de trabajo Pymol en la carpeta: " + input_path)
+    logging.info("Puede utilizarlo para ver en detalle las estructuras alineadas")
+
     pymol_label = Label(secondFrame,text="Alineamiento de estructuras terciarias. Para verlo en detalle abrir el archivo .pse en la carpeta de la ejecucion actual.")
     pymol_label.pack(pady=(0,30))
     loadPymol = Image.open(input_path + "/" + pdb_id + ".png")
     renderPymol = ImageTk.PhotoImage(loadPymol)
     imgPymol = Label(secondFrame,image=renderPymol)
     imgPymol.image = renderPymol
-    imgPymol.pack(pady = (15, 0))
+    imgPymol.pack(pady = (15, 0)) 
+
+    #Finalizacion de la busqueda
+    delete_unused_files(input_path)
+    search_label.config(text="Búsqueda finalizada")
+    progress_bar.stop()
+
+#Actualiza la progressbar al valor indicado
+def define_progress(value):
+    progress_bar.step(value)
+    root.update_idletasks()
+
+#Se eliminan los archivos que ya fueron utilizados y no tienen utilidad para el usuario
+def delete_unused_files(input_path):
+    root.attributes("-fullscreen", True)
+    root.update_idletasks()
     files = os.listdir("./")
     for file in files:
         if(file.endswith(".pdb") or file.endswith(".fasta") or file.endswith(".cif") or file.endswith(".xml")):
@@ -276,9 +242,10 @@ def generate_3structure(pdb_id,input_path):
     files = os.listdir(input_path)
     for file in files:
         if(file.endswith(".pdb") or file.endswith(".png")):
-            os.remove(os.path.join(input_path,file))  
-    search_label.config(text="Busqueda finalizada")
-    progress_bar.stop()
+            os.remove(os.path.join(input_path,file)) 
+
+
+#UI principal
 
 pdbLabel = Label(secondFrame, text="Ingresar código PDB")
 pdbLabel.pack()
---
+++ services/logomaker_service.py
@@ -0,0 +1,47 @@
+import logomaker as lm
+from tkinter import *
+import numpy as np
+import math
+from tkinter import messagebox, ttk
+import matplotlib.pyplot as plt
+from PIL import Image,ImageTk
+import logging
+
+def primary_structure_conservation(seqs,pdb_id,frame,logger):
+    counts_mat = lm.alignment_to_matrix(seqs)
+    divider = len(seqs[0]) / 35
+    logger.info("Se divide las cadenas de las estructuras primarias en " + str(math.ceil(divider)) + " partes para la generacion de graficos con LogoMaker")
+    counts_mat_list = np.array_split(counts_mat, math.ceil(divider))
+    alignment_label = Label(frame,text="Alineamiento de estructura primaria")
+    alignment_label.pack(pady=(0,30))
+    for df in counts_mat_list:
+        crp_logo = lm.Logo(df,color_scheme='skylign_protein')
+        # style using Axes methods
+        crp_logo.ax.xaxis.set_ticks_position('none')
+        crp_logo.ax.xaxis.set_tick_params(pad=-1)
+        plt.savefig( pdb_id + "_aln.png")
+        load = Image.open(pdb_id + "_aln.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(frame,image=render)
+        img.image = render
+        img.pack(pady = (30, 0))
+
+
+def secondary_structure_conservation(seqs2,pdb_id,frame,logger):
+    counts_mat2 = lm.alignment_to_matrix(seqs2)
+    divider2= len(seqs2[0]) / 40
+    logger.info("Se divide las cadenas de las estructuras secundarias en " + str(math.ceil(divider2)) + " partes para la generacion de graficos con LogoMaker")
+    counts_mat_list2 = np.array_split(counts_mat2, math.ceil(divider2))
+    alignment_label2 = Label(frame,text="Alineamiento de estructura secundaria")
+    alignment_label2.pack(pady=(0,30))
+    for df in counts_mat_list2:
+        crp_logo = lm.Logo(df)
+        # style using Axes methods
+        crp_logo.ax.xaxis.set_ticks_position('none')
+        crp_logo.ax.xaxis.set_tick_params(pad=-1)
+        plt.savefig(pdb_id + "_aln_secondary.png")
+        load = Image.open(pdb_id + "_aln_secondary.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(frame,image=render)
+        img.image = render
+        img.pack(pady = (30, 0))
